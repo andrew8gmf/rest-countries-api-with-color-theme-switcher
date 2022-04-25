5cc8839fb4d7a46824317e0e10aefe2f9775dbd8
Andrew <andrew8gmf@gmail.com>
2022-04-25 15:55:28 -0300

    feat: add detail page initial structure

--- src/app/country/detail/detail.component.ts
+++ src/app/country/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../country.service';
 
 @Component({
   selector: 'app-detail',
@@ -9,11 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailComponent implements OnInit {
 
   name;
+  country = {};
+  render:boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit() {
     this.name = this.route.snapshot.paramMap.get('name');
+    this.getCountry();
+  }
+
+  async getCountry() {
+    await this.countryService.getName(this.name).subscribe(countries => {
+      this.country = countries[0];
+      this.render = true;
+
+      return this.country, this.render;
+    });
   }
 
 }
